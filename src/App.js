25e9ae242896987a8c51480e74a15674cd5b442d
CosimoL <cosimo.lor@gmail.com>
2021-06-26 12:00:04 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import MoviesHomepage from './pages/homepage';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='header'>
+        <span>MOVIES REGISTRY</span>
+      </div>
+      <MoviesHomepage />
+    </div>
+  );
+}
+
+export default App;
